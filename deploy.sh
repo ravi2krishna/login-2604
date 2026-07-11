a938cbf9cd75ade0579f76c9396d2de0c1505721
sgabrar <sgabrar@gmail.com>
2026-07-11 18:44:37 +0000

    create deployment script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#use this script to deploy application -login app
+
+
+sudo apt update -y
+sudo apt install zip unzip -y
+sudo apt install nginx -y
+sudo rm -rf /var/www/html
+sudo git clone https://github.com/ravi2krishna/login-2604.git /var/www/html
+
